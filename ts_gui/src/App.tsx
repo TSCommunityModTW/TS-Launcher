0fd087f04fb4c3cada1a79b46d10e62b7a66ac22
Lun <chupks123@gmail.com>
2024-01-22 19:31:45 +0800

    Update code

--- ts_gui/src/App.tsx
+++ ts_gui/src/App.tsx
@@ -23,7 +23,6 @@ export default function App() {
         setTimeout(setupAppWindow, 200);
 
         await Launcher.initialize();
-        // HI
 
         // navigate("/instanceSettings/nr-server/parameters");
         navigate("/main/namelessrealms/server_info")
