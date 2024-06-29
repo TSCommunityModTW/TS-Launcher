f8c211959bf62ea8addddd56e15a8b24eb3dd621
martin1194 <martinc1194@gmail.com>
2024-06-29 12:53:56 +0800

    Add Skin View in home page

--- ts_gui/src/pages/main/pages/home/Home.tsx
+++ ts_gui/src/pages/main/pages/home/Home.tsx
@@ -1,10 +1,33 @@
 import styles from "./Home.module.scss";
+import ReactSkinview3d from "react-skinview3d"
+import { WalkingAnimation } from "skinview3d";
+
+import { useLoaderData } from "react-router-dom";
+import { IMainLoader } from "@/loader";
+
 
 export default function Home() {
-    
+
+    const loaderData = useLoaderData() as IMainLoader;
     return (
         <div className={styles.homeContainer}>
-            <h1 className={styles.text}>功能尚未完成</h1>
+          <h1 className={styles.text}>功能尚未完成</h1>
+          {loaderData?.player?.uuid ? (
+            <ReactSkinview3d
+              skinUrl={`https://crafatar.com/skins/${loaderData.player.uuid}`}
+              height="500"
+              width="500"
+              onReady={({ viewer }) => {
+                const walkingAnimation = new WalkingAnimation();
+                walkingAnimation.headBobbing = false;
+                viewer.animation = walkingAnimation;
+                viewer.animation.speed = 0.5;
+              }}
+            />
+          ) : (
+            <p>無法加載皮膚</p>
+            )}
+            
         </div>
-    )
+      );
 }
