29d511011a1004220b65837c8a62ac6fe691215e
martin1194 <martinc1194@gmail.com>
2024-07-02 00:07:12 +0800

    Add button in main menu (non-finish)

--- ts_gui/src/pages/main/pages/home/Home.tsx
+++ ts_gui/src/pages/main/pages/home/Home.tsx
@@ -3,12 +3,15 @@ import ReactSkinview3d from "react-skinview3d"
 import { WalkingAnimation } from "skinview3d";
 
 import { useLoaderData } from "react-router-dom";
-import { IMainLoader } from "@/loader";
+import { IMainLoader,IServerSelectedLoader, serverselectedLoader} from "@/loader";
+import ButtonPlay from "../../components/buttonPlay/ButtonPlay";
 
 
 export default function Home() {
 
+
     const loaderData = useLoaderData() as IMainLoader;
+    const serverSelectedLoader = useLoaderData() as IServerSelectedLoader;
     return (
         <div className={styles.homeContainer}>
           <h1 className={styles.text}>功能尚未完成</h1>
@@ -26,6 +29,14 @@ export default function Home() {
             />
           ) : (
             <p>無法加載皮膚</p>
+            )}
+            {serverSelectedLoader?.server_id?(
+              <ButtonPlay
+                  serverId={serverSelectedLoader?.server_id}
+                  childrenServerId={serverSelectedLoader?.childrenServerId}
+              />
+            ):(
+              <p>沒有最後遊玩的伺服器</p>
             )}
             
         </div>
