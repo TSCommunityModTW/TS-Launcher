f24d6d350e8f58588b47bfe2ae82778dac5bb817
martin1194 <martinc1194@gmail.com>
2024-07-06 03:30:07 +0800

    Some UI change

--- ts_gui/src/pages/main/pages/home/Home.tsx
+++ ts_gui/src/pages/main/pages/home/Home.tsx
@@ -5,59 +5,69 @@ import { WalkingAnimation } from "skinview3d";
 import { useLoaderData } from "react-router-dom";
 import { IMainLoader, IServerSelectedLoader, serverselectedLoader } from "@/loader";
 import ButtonPlay from "../../components/buttonPlay/ButtonPlay";
+import { ILauncherAssetsServer } from "@/interfaces/ILauncherAssetsServer";
+import { ILauncherAssetsServerChildren } from "@/interfaces/ILauncherAssetsServerChildren";
 
 
 export default function Home() {
 
 
+  interface ILauncherAssetsServerResult {
+    server: ILauncherAssetsServer;
+    child: ILauncherAssetsServerChildren;
+  }
   const loaderData = useLoaderData() as IMainLoader;
-  function findChildImageUrlById(childId: string): string | null {
+  function findChildServerById(childId: String): ILauncherAssetsServerResult | null {
     for (const server of loaderData.servers) {
       const child = server.children.find(child => child.id === childId);
       if (child) {
-        return child.imageUrl;
+        return { server, child };
       }
     }
     return null;
   }
-  const imgsrc = findChildImageUrlById(loaderData?.selected_server?.childrenServerId);
+  const SelectedServer = findChildServerById(loaderData?.selected_server?.childrenServerId);
+  const imgsrc = SelectedServer?.child?.imageUrl;
   return (
     <div className={styles.homeContainer}>
-      <h1 className={styles.text}>功能尚未完成</h1>
-      {loaderData?.player?.uuid ? (
-        <ReactSkinview3d
-          skinUrl={`https://crafatar.com/skins/${loaderData.player.uuid}`}
-          height="500"
-          width="500"
-          onReady={({ viewer }) => {
-            const walkingAnimation = new WalkingAnimation();
-            walkingAnimation.headBobbing = false;
-            viewer.animation = walkingAnimation;
-            viewer.animation.speed = 0.5;
-          }}
-        />
-      ) : (
-        <p>無法加載皮膚</p>
-      )}
-      {imgsrc ? (
-        <div>
+
+
+      {SelectedServer ? (
+        <div className={styles.serverBorderContainer}>
           <img className={styles.serverBorderContainer} src={imgsrc} />
         </div>
       ) : (
         <p></p>
       )}
 
-      {loaderData?.selected_server ? (
-        <div>
-          <ButtonPlay
-            serverId={loaderData?.selected_server?.server_id}
-            childrenServerId={loaderData?.selected_server?.childrenServerId}
+      <div>
+        {loaderData?.player?.uuid ? (
+          <ReactSkinview3d
+            skinUrl={`https://crafatar.com/skins/${loaderData.player.uuid}`}
+            height="500"
+            width="500"
+            onReady={({ viewer }) => {
+              const walkingAnimation = new WalkingAnimation();
+              walkingAnimation.headBobbing = false;
+              viewer.animation = walkingAnimation;
+              viewer.animation.speed = 0.5;
+            }}
           />
-        </div>
-      ) : (
-        <p>沒有最後遊玩的伺服器</p>
-      )}
+        ) : (
+          <p>無法加載皮膚</p>
+        )}
+        {loaderData?.selected_server ? (
+          <div className={styles.buttonPlay}>
+            <ButtonPlay
+              serverId={loaderData?.selected_server?.server_id}
+              childrenServerId={loaderData?.selected_server?.childrenServerId}
+            />
+          </div>
+        ) : (
+          <p>沒有最後遊玩的伺服器</p>
+        )}
+      </div>
 
-    </div>
+    </div >
   );
 }
