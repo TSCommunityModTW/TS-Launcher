cbc10bda011714652fe5e8aae56a197e55d9de80
martin1194 <martinc1194@gmail.com>
2024-07-15 12:30:15 +0800

    Remove un-use code

--- ts_gui/src/invoke/logger.ts
+++ ts_gui/src/invoke/logger.ts
@@ -6,7 +6,6 @@ import { invoke } from '@tauri-apps/api/tauri';
 // Function to log messages
 export default class logger {
     public static async logMessage(level: string, message: string) {
-        invoke('log_message', { level, message })
         await invoke("plugin:logger|log_message", { level, message })
             .then(() => {
                 console.log(`Log [${level}] sent to Rust backend successfully.`);
