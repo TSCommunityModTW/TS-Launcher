30ee53263ffc3d5f991b2b9671ce23980ee9a37b
yucheng <quasipersonal58@gmail.com>
2024-10-24 19:32:57 +0800

    apply_blur to apply_mica

--- ts_gui/src-tauri/src/vibrancy.rs
+++ ts_gui/src-tauri/src/vibrancy.rs
@@ -1,5 +1,5 @@
 use tauri::{App, Manager};
-use window_vibrancy::{apply_blur, apply_mica, apply_vibrancy, NSVisualEffectMaterial, NSVisualEffectState};
+use window_vibrancy::{apply_mica, apply_vibrancy, NSVisualEffectMaterial, NSVisualEffectState};
 
 /// Init vibrancy
 pub fn init(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
@@ -10,8 +10,8 @@ pub fn init(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
     apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, Some(NSVisualEffectState::Active), None).expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
     #[cfg(target_os = "windows")]
-    // apply_mica(&window, None).expect("Unsupported platform! 'apply_mica' is only supported on Windows");
-    apply_blur(&window, Some((18, 18, 18, 125))).expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+    apply_mica(&window, None).expect("Unsupported platform! 'apply_mica' is only supported on Windows");
+    // apply_blur(&window, Some((18, 18, 18, 125))).expect("Unsupported platform! 'apply_blur' is only supported on Windows");
 
     Ok(())
 }
